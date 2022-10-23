e81e17d572b754935b849aedeffb15582458b4c5
a.karamiani <aziz.karamiani@gmail.com>
2022-10-23 16:28:27 +0330

    Add tag model and tests

---
+++ tests/Feature/Models/TagsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Tag;
+
+class TagsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $tag = Tag::factory()->create();
+
+        $this->assertDatabaseHas('tags', $tag->toArray());
+    }
+}
