2cc8830bf92d260affad6034d69d34cb71d4aee2
a.karamiani <aziz.karamiani@gmail.com>
2022-10-26 16:46:43 +0330

    Home page test for user and admin

---
+++ tests/Feature/Views/HomepageViewTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature\Views;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomepageViewTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_home_page_admin_user()
+    {
+        $user = User::factory()->state(['type' => 'admin'])->create();
+        $this->actingAs($user);
+
+        $view = $this->view('home');
+        $view->assertSee('<a href="/admin/dashboard" class="btn btn-primary">Admin Dashboard</a>', false);
+    }
+
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_home_page_normal_user()
+    {
+        $user = User::factory()->state(['type' => 'user'])->create();
+        $this->actingAs($user);
+
+        $view = $this->view('home');
+        $view->assertDontSee('<a href="/admin/dashboard" class="btn btn-primary">Admin Dashboard</a>', false);
+    }
+}
